8c0349fcaa2b9144d0c4e7b1bd9915e02aa356ed
Esam Isbiga <50238782+gameskings064@users.noreply.github.com>
2021-05-19 19:39:20 +0200

    Add files via upload

---
+++ Config.php
@@ -0,0 +1,7 @@
+<?php
+   define('DB_SERVER', 'sql6.freesqldatabase.com');
+   define('DB_USERNAME', 'sql6413449');
+   define('DB_PASSWORD', 'MUMaEMgnDv');
+   define('DB_DATABASE', 'sql6413449');
+   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+?>
